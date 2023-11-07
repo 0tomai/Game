f38cda1ed8989f92aae06d2b47d131e04ac685cc
0tomai <87027721+0tomai@users.noreply.github.com>
2023-11-07 18:03:07 +0100

    Update reader.c

--- src/reader.c
+++ src/reader.c
@@ -3,50 +3,42 @@
 #include <SDL2/SDL.h>
 #include "reader.h"
 #include "image.h"
-//fontion taille fichier
 
-//fonction remplir tableau
-
-void read(SDL_Renderer* renderer) {
-
+void tailleFichier(int* nbCol, int* maxLigne)
+{
+    int taille = 0;
+    int max = 0;
+    int colone = 0;
     char c;
     FILE* f;
     f = fopen("src/map.txt", "r");
-    /*c = fgetc(f);
-    while (c != EOF) {
-         printf("%c", c);
-        c = fgetc(f);
 
-         
-    }
-    exit(0);*/
-    int x = 0; 
-    int y = 0; 
+    c=fgetc(f);
 
-    while ((c = fgetc(f)) != EOF) {
-        if (c == '1') {
-            
-            // Charger et afficher l'image
-            SDL_Texture* image = charger_image("src/font.bmp", renderer);
-
-            // Afficher l'image à la position x, y
-            SDL_Rect destRect = {x, y, 0, 0}; 
-            SDL_QueryTexture(image, NULL, NULL, &destRect.w, &destRect.h);
-            SDL_RenderCopy(renderer, image, NULL, &destRect);
-
-            x += destRect.w; 
-
-            SDL_DestroyTexture(image);
-        } else if (c == '\n') {
-            x = 0;
-            y += 100;
-        } else 
+    while(c != EOF)
+    {
+        taille ++;
+        if(c == '\n')
         {
-            x = 0;
-            y += 100;
+            colone ++;
+            if(max < taille)
+            {
+                max = taille;
+            }
+            taille = 0;
         }
+        c = fgetc(f);
     }
-    
+    max = max-1;
+    colone = colone-1;
+    *nbCol = colone;
+    *maxLigne = max;
+}
 
-    fclose(f);
+
+
+void read(SDL_Renderer* renderer) 
+{
+
+
 }
